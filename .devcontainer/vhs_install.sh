bb4f29974fc2b69c7ab3bafa20e5e4ed1ecbdcda
Mr. Coxall <mr.coxall@mths.ca>
2024-06-03 21:32:22 +0000

    add vhs

---
+++ .devcontainer/vhs_install.sh
@@ -0,0 +1,16 @@
+sudo apt-get update
+sudo apt upgrade -y
+wget https://github.com/charmbracelet/vhs/releases/download/v0.7.2/vhs_0.7.2_amd64.deb
+sudo mkdir /usr/share/fonts/truetype/ComicMono
+sudo cp ./.devcontainer/ComicMono.ttf /usr/share/fonts/truetype/ComicMono
+# install ttyd
+sudo apt-get install -y build-essential cmake git libjson-c-dev libwebsockets-dev
+git clone https://github.com/tsl0922/ttyd.git
+cd ttyd && mkdir build && cd build
+cmake ..
+make && sudo make install
+cd ..
+sudo apt install ffmpeg -y
+sudo rm -r ./ttyd
+sudo apt install ./vhs_0.7.2_amd64.deb -y
+sudo rm ./vhs_0.7.2_amd64.deb
