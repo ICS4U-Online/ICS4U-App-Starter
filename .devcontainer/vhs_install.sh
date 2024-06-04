a4f83e5bd7e342d97b513406040ec2672ee95564
Patrick Coxall <mr.coxall@mths.ca>
2024-06-04 18:17:58 -0400

    Update vhs_install.sh

--- .devcontainer/vhs_install.sh
+++ .devcontainer/vhs_install.sh
@@ -2,7 +2,7 @@ sudo apt-get update
 sudo apt upgrade -y
 wget https://github.com/charmbracelet/vhs/releases/download/v0.7.2/vhs_0.7.2_amd64.deb
 sudo mkdir /usr/share/fonts/truetype/ComicMono
-sudo cp ./.devcontainer/ComicMono.ttf /usr/share/fonts/truetype/ComicMono
+sudo cp ./ComicMono.ttf /usr/share/fonts/truetype/ComicMono
 # install ttyd
 sudo apt-get install -y build-essential cmake git libjson-c-dev libwebsockets-dev
 git clone https://github.com/tsl0922/ttyd.git
